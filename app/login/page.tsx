83865302576c1d78cee1ed71ea03dc811023f420
jhosuapp <jhosuapvll@gmail.com>
2026-05-13 11:28:50 -0500

    config: initial

---
+++ app/login/page.tsx
@@ -0,0 +1,7 @@
+export default function Login() {
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae enim iure totam ipsa debitis est sed velit voluptatibus! Repellat voluptatibus culpa officia sed similique! Ducimus adipisci unde ab sed aspernatur?
+    </div>
+  );
+}
